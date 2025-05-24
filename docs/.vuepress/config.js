d0bc2ba605ddf0d80638183c4a01bbf0562fbe77
X4M7 <1415808154@qq.com>
2025-05-24 09:38:29 +0800

    添加base

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,7 @@ import { defineUserConfig } from 'vuepress'
 import { viteBundler } from '@vuepress/bundler-vite'
 
 export default defineUserConfig({
+  base: '/InternshipGain/',
   lang: 'en-US',
 
   title: 'VuePress',
