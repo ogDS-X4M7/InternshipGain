4215c19d7a3adc5ad3c57e0ced66a5bef5bae42b
X4M7 <1415808154@qq.com>
2025-05-25 23:18:53 +0800

    完成目前一段实习记得的所有收获记录

--- docs/.vuepress/.temp/pages/guide/theFirstInternship/gain1.html.vue
+++ docs/.vuepress/.temp/pages/guide/theFirstInternship/gain1.html.vue
@@ -16,12 +16,12 @@
 <div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0"><div class="line-number"></div></div></div><p>移动到分支</p>
 <div class="language-text line-numbers-mode" data-highlighter="prismjs" data-ext="text"><pre v-pre><code><span class="line">git checkout branchName</span>
 <span class="line"></span></code></pre>
-<div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0"><div class="line-number"></div></div></div><p>暂存更改（不过我一般直接使用vscode的源代码管理暂存更改,这里的/xx/yy是要写所要暂存更改的根目录开始的文件路径）</p>
+<div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0"><div class="line-number"></div></div></div><p>暂存更改（不过我一般直接使用<em>vscode</em>的源代码管理暂存更改,这里的<code v-pre>/xx/yy</code>是要写所要暂存更改的根目录开始的文件路径）</p>
 <div class="language-text line-numbers-mode" data-highlighter="prismjs" data-ext="text"><pre v-pre><code><span class="line">git add /xx/yy</span>
 <span class="line">另外，暂存所有更改可以使用.，写法如下：</span>
 <span class="line">git add .</span>
 <span class="line"></span></code></pre>
-<div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>提交更改（我也是更爱用vscode的源代码管理提交更改，&quot;&quot;中写关于你本次提交修改内容的描述）</p>
+<div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>提交更改（我也是更爱用<em>vscode</em>的源代码管理提交更改，&quot;&quot;中写关于你本次提交修改内容的描述）</p>
 <div class="language-text line-numbers-mode" data-highlighter="prismjs" data-ext="text"><pre v-pre><code><span class="line">git commit -m ""</span>
 <span class="line">-am可以把暂存和提交一起执行，等价于 git add . + git commit，写法如下：</span>
 <span class="line">git commit -am "Update files" </span>
@@ -30,26 +30,26 @@
 <div class="language-text line-numbers-mode" data-highlighter="prismjs" data-ext="text"><pre v-pre><code><span class="line">git pull</span>
 <span class="line"></span></code></pre>
 <div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0"><div class="line-number"></div></div></div><p>指令中还有一些常见的内容，比如参数、别名</p>
-<p>-a：all的意思</p>
+<p><code v-pre>-a</code>：all的意思</p>
 <div class="language-text line-numbers-mode" data-highlighter="prismjs" data-ext="text"><pre v-pre><code><span class="line">查看所有分支：本地+远程</span>
 <span class="line">git branch -a</span>
 <span class="line"></span></code></pre>
-<div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0"><div class="line-number"></div><div class="line-number"></div></div></div><p>-b：创建并切换分支,在创建新分支的同时切换到该分支</p>
+<div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0"><div class="line-number"></div><div class="line-number"></div></div></div><p><code v-pre>-b</code>：创建并切换分支,在创建新分支的同时切换到该分支</p>
 <div class="language-text line-numbers-mode" data-highlighter="prismjs" data-ext="text"><pre v-pre><code><span class="line">git checkout -b new-feature</span>
 <span class="line"></span></code></pre>
 <div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0"><div class="line-number"></div></div></div><p><a id="del-branch"></a>
--d：删除分支</p>
+<code v-pre>-d</code>：删除分支</p>
 <div class="language-text line-numbers-mode" data-highlighter="prismjs" data-ext="text"><pre v-pre><code><span class="line">git branch -d new-feature</span>
 <span class="line"></span></code></pre>
-<div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0"><div class="line-number"></div></div></div><p>origin:是远程仓库的默认别名,如果要提交或拉取远程仓库代码，远程仓库的名称(或别名)是必不可少的</p>
+<div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0"><div class="line-number"></div></div></div><p><code v-pre>origin</code>：是远程仓库的默认别名,如果要提交或拉取远程仓库代码，远程仓库的名称(或别名)是必不可少的</p>
 <div class="language-text line-numbers-mode" data-highlighter="prismjs" data-ext="text"><pre v-pre><code><span class="line">git push origin main</span>
 <span class="line">git pull origin main</span>
 <span class="line"></span></code></pre>
 <div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0"><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="云效日常使用" tabindex="-1"><a class="header-anchor" href="#云效日常使用"><span>云效日常使用</span></a></h2>
-<p>云效仓库日常使用的话，完成个人代码任务后，给云效管理平台对应项目创建个人分支，然后在本地这边也是一样新建分支并移动到新分支下，本地代码push提交到新的分支上去，申请合并就可以了</p>
+<p>云效仓库日常使用的话，完成个人代码任务后，给云效管理平台对应项目创建个人分支，然后在本地这边也是一样新建分支并移动到新分支下，本地代码<code v-pre>push</code>提交到新的分支上去，申请合并就可以了</p>
 <h2 id="多写多用" tabindex="-1"><a class="header-anchor" href="#多写多用"><span>多写多用</span></a></h2>
 <p>难度并不大，主要是作为学生大多数时候都是个人开发，所以缺乏团队开发，共享开发的经验。只要多用到自然就会了。</p>
-<p>包括像现在正在写的这个文档项目，从初始代码就开始提交部署到github，期间更改、新增代码都会使用到git命令，也同样可以学习、熟悉git指令。</p>
+<p>包括像现在正在写的这个文档项目，从初始代码就开始提交部署到<code v-pre>github</code>，期间更改、新增代码都会使用到<code v-pre>git</code>命令，也同样可以学习、熟悉<code v-pre>git</code>指令。</p>
 </div></template>
 
 
