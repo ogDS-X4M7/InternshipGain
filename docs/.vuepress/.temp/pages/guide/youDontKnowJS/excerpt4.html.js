fd632891130e360fdf44073a0bcaffeb253a1bb7
X4M7 <1415808154@qq.com>
2026-07-21 23:43:23 +0800

    增加this与对象原型摘录第四、五章内容

---
+++ docs/.vuepress/.temp/pages/guide/youDontKnowJS/excerpt4.html.js
@@ -0,0 +1,16 @@
+import comp from "D:/A-Front-end/A-Vuepress/InternshipGain/docs/.vuepress/.temp/pages/guide/youDontKnowJS/excerpt4.html.vue"
+const data = JSON.parse("{\"path\":\"/guide/youDontKnowJS/excerpt4.html\",\"title\":\"你不知道的JS-摘录-this与对象原型-3\",\"lang\":\"zh-CN\",\"frontmatter\":{},\"headers\":[{\"level\":2,\"title\":\"摘自第五章\",\"slug\":\"摘自第五章\",\"link\":\"#摘自第五章\",\"children\":[]},{\"level\":2,\"title\":\"摘自第五章-[[Prototype]]\",\"slug\":\"摘自第五章-prototype\",\"link\":\"#摘自第五章-prototype\",\"children\":[{\"level\":3,\"title\":\"Object.prototype\",\"slug\":\"object-prototype\",\"link\":\"#object-prototype\",\"children\":[]},{\"level\":3,\"title\":\"设置与遮蔽属性\",\"slug\":\"设置与遮蔽属性\",\"link\":\"#设置与遮蔽属性\",\"children\":[]}]},{\"level\":2,\"title\":\"摘自第五章-“类”\",\"slug\":\"摘自第五章-类\",\"link\":\"#摘自第五章-类\",\"children\":[{\"level\":3,\"title\":\"“类”函数\",\"slug\":\"类-函数\",\"link\":\"#类-函数\",\"children\":[]}]},{\"level\":2,\"title\":\"摘自第五章-“（原型）继承”\",\"slug\":\"摘自第五章-原型-继承\",\"link\":\"#摘自第五章-原型-继承\",\"children\":[{\"level\":3,\"title\":\"考察“类”关系\",\"slug\":\"考察-类-关系\",\"link\":\"#考察-类-关系\",\"children\":[]}]},{\"level\":2,\"title\":\"摘自第五章-对象链接\",\"slug\":\"摘自第五章-对象链接\",\"link\":\"#摘自第五章-对象链接\",\"children\":[{\"level\":3,\"title\":\"创建链接\",\"slug\":\"创建链接\",\"link\":\"#创建链接\",\"children\":[]}]},{\"level\":2,\"title\":\"摘自第五章-复习\",\"slug\":\"摘自第五章-复习\",\"link\":\"#摘自第五章-复习\",\"children\":[]}],\"git\":{},\"filePathRelative\":\"guide/youDontKnowJS/excerpt4.md\"}")
+export { comp, data }
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -53,7 +53,7 @@ export default defineUserConfig({
           text: '你不知道的JS摘录',
           collapsible: true,
           prefix: 'youDontKnowJS/',
-          children: ['index', 'excerpt1', 'excerpt2', 'excerpt3'],
+          children: ['index', 'excerpt1', 'excerpt2', 'excerpt3', 'excerpt4'],
         },
         {
           text: 'React + Taro 小程序简单学习',
